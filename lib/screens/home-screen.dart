0a639e4d9740a30d5551b29c72b6f2934c38c1e4
PazJulia <paz.julia7@gmail.com>
2022-09-22 21:39:34 -0300

    adicionando menu inferior

--- lib/screens/home-screen.dart
+++ lib/screens/home-screen.dart
@@ -5,11 +5,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _body(context),
       backgroundColor: const Color.fromARGB(255, 238, 238, 238),
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text('TESTE', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.white,
+        title: const Text('TESTE', style: TextStyle(color: Colors.black),),
+      ),
+      body: _body(context),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.emoji_events_outlined, size: 30), label: 'CONQUISTAS'),
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined, size: 30), label: 'INÍCIO'),
+          BottomNavigationBarItem(icon: Icon(Icons.leaderboard_outlined, size: 30), label: 'CLASSIFICAÇÃO')
+        ],
       ),
     );
   }
