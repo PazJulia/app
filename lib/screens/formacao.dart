715622a1d9bbed5f63cdedbc80d7bcd567b4547a
PazJulia <paz.julia7@gmail.com>
2022-10-15 00:37:15 -0300

    implementando card de atividade

--- lib/screens/formacao.dart
+++ lib/screens/formacao.dart
@@ -6,26 +6,68 @@ class FormacaoCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              shadowColor: const Color.fromARGB(10, 0, 0, 0),
-              color: const Color.fromARGB(255, 195, 219, 255),
-              elevation: 2,
-              child: const SizedBox(
-                width: double.infinity,
-                height: 100,
-                child: Center(child: Text('TESTE CARD DE ATIVIDADES')),
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            shadowColor: const Color.fromARGB(10, 0, 0, 0),
+            color: const Color.fromARGB(255, 195, 219, 255),
+            clipBehavior: Clip.hardEdge,
+            elevation: 2,
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 25,
+                          height: 50,
+                          width: 100,
+                          child: Container(
+                            color: Colors.pink,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    color: Colors.black,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20, top: 15, right: 20, bottom: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Título Módulo',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Teste de descrição de módulooooooooooooooooooooooooooooooooooooo',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
