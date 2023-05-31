eb5df19412924b19bb9c34f64e835ad60523a061
PazJulia <paz.julia7@gmail.com>
2023-05-30 21:17:37 -0300

    Update matricula-controller.dart

--- lib/core/domain/matricula/matricula-controller.dart
+++ lib/core/domain/matricula/matricula-controller.dart
@@ -1,5 +1,6 @@
 import 'package:app/core/api/api-service.dart';
 import 'package:app/core/domain/matricula/matricula.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../shared/functions/get-current-date-time-utc.dart';
 
@@ -7,8 +8,10 @@ class MatriculaController extends ApiService {
   Future<Matricula> getMatricula() async {
     String dataHora = getCurrentDateTimeUtc();
     String encodedDataHora = Uri.encodeComponent(dataHora);
+    var prefs = await SharedPreferences.getInstance();
+    int? linguagemId = (prefs.getInt("language"));
 
-    Map<String, dynamic> response = await authorizedGet('matriculas/getByToken/$encodedDataHora');
+    Map<String, dynamic> response = await authorizedGet('matriculas/getByToken/$encodedDataHora/$linguagemId');
 
     return Matricula.fromJson(response);
   }
