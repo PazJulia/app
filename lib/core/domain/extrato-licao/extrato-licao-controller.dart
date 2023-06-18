761367d4b4d88bfeccba27226addd6f20b402288
PazJulia <paz.julia7@gmail.com>
2023-06-18 19:15:16 -0300

    Update extrato-licao-controller.dart

--- lib/core/domain/extrato-licao/extrato-licao-controller.dart
+++ lib/core/domain/extrato-licao/extrato-licao-controller.dart
@@ -4,11 +4,14 @@ import 'package:app/core/domain/extrato-licao/extrato-licao.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:app/core/api/api-service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ExtratoLicaoController extends ApiService {
   Future<ExtratoLicao> postExtratoLicao(ExtratoLicao extrato) async {
+    var prefs = await SharedPreferences.getInstance();
+    int? linguagemId = (prefs.getInt("language"));
     Map<String, dynamic> requestBody = extrato.toJson();
-    http.Response response = await authorizedPost('extratoslicao', requestBody);
+    http.Response response = await authorizedPost('extratoslicao/$linguagemId', requestBody);
 
     if (response.statusCode == 201) {
       final Map<String, dynamic> data = json.decode(response.body);
